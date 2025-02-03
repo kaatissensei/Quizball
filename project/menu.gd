a12e7d229aebe8a1a1239441ad6b067458b404d1
Kaatisu-sensei <kaatissensei@gmail.com>
2025-02-03 14:04:33 +0900

    Added FileAccessWeb project files

--- project/menu.gd
+++ project/menu.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+@onready var upload_button: Button = %"Upload Button" as Button
+@onready var progress: ProgressBar = %"Progress Bar" as ProgressBar
+@onready var success_label: Label = %"Success Label" as Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	upload_button.pressed.connect(_on_upload_pressed)
+	file_access_web.load_started.connect(_on_file_load_started)
+	file_access_web.loaded.connect(_on_file_loaded)
+	file_access_web.progress.connect(_on_progress)
+	file_access_web.error.connect(_on_error)
 
 
 func _open_fileDialog():
@@ -14,9 +21,29 @@ func _on_file_dialog_file_selected(path: String) -> void:
 	Main.parse_csv()
 	get_tree().change_scene_to_file("res://Quiz_scene.tscn")
 
-	#var csvTxt = Main.csvFile.get_as_text()
-	#print (csvTxt)
+#WEB FILE ACCESS FUNCTIONS ------------------------------------------------\
 
+var file_access_web: FileAccessWeb = FileAccessWeb.new()
+
+func _on_file_load_started(file_name: String) -> void:
+	progress.visible = true
+	success_label.visible = false
+
+func _on_upload_pressed() -> void:
+	file_access_web.open()
+
+func _on_progress(current_bytes: int, total_bytes: int) -> void:
+	var percentage: float = float(current_bytes) / float(total_bytes) * 100
+	progress.value = percentage
+
+func _on_file_loaded(file_name: String, type: String, base64_data: String) -> void:
+	progress.visible = false
+	#success_label.visible = true
+	_start()
+
+func _on_error() -> void:
+	push_error("Error!")
+#--------------------------------------------------------------------/
 
 func _on_button_2_pressed() -> void:
 	Main.parse_csv()
